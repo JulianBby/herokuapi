e4da6a76aac323102426ba8454e03f4b97e08c6a
Camilo Orduz <acorduz9@misena.edu.co>
2022-08-15 18:20:20 -0500

    Intento

--- basededatos/conexio3.php
+++ basededatos/conexio3.php
@@ -1,7 +1,15 @@
 <?php
 
-$mysqli = new mysqli("bkc3739td52nqn13e3iu-mysql.services.clever-cloud.com", "u85yrqverx1h8saf", "u85yrqverx1h8saf", "bkc3739td52nqn13e3iu", 3306);
-if ($mysqli ->connect_errno){
-    echo "Fallo al conectar a MYSQL: (". $mysqli->connect_errno . ") " . $mysqli->connect_errno;
+$hostname = "bkc3739td52nqn13e3iu-mysql.services.clever-cloud.com";
+$username = "u85yrqverx1h8saf";
+$password = "M30qXRJgkOtmmkmOjwvS";
+$dbname = "bkc3739td52nqn13e3iu";
+
+$db = new mysqli($hostname, $username, $password, $dbname, 3306);
+
+
+if ($db->connect_errno){
+    echo "Fallo la conexión a la base de datos";
 }
 
+?>
