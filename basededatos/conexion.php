c390edae2c8864831d250b39f438db97ba8738fc
Camilo Orduz <acorduz9@misena.edu.co>
2022-08-18 19:35:21 -0500

    a

--- basededatos/conexion.php
+++ basededatos/conexion.php
@@ -1,17 +1,9 @@
-<?php 
-class DB{
-    
-    private function __construct(){}
-
-    public static function conectar(){
-        $localhost = 'bkc3739td52nqn13e3iu-mysql.services.clever-cloud.com';
-        $database = 'bkc3739td52nqn13e3iu';
-        $username = 'u85yrqverx1h8saf';
-        $password = 'M30qXRJgkOtmmkmOjwvS';
-        
-        $conexion = new PDO('mysql:host='+ $localhost + '; dbname=' + $database, $username, $password, array(
-            PDO::ATTR_PERSISTENT => true));
-        return $conexion; 
-    }
+<?php
+    $conexion = new mysqli('bkc3739td52nqn13e3iu-mysql.services.clever-cloud.com','u85yrqverx1h8saf', 'M30qXRJgkOtmmkmOjwvS', 'bkc3739td52nqn13e3iu', 3306);
+//comprobar conexión
+if(mysqli_connect_errno()){
+    printf("Fallo la conexión");
+}else{
+    printf("Estas conectado");
 }
 ?>
