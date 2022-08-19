f48e4377fafab87fa61cca57fd8ca1db9db8e363
Camilo Orduz <acorduz9@misena.edu.co>
2022-08-18 20:01:47 -0500

    actual

--- registro.php
+++ registro.php
@@ -10,8 +10,8 @@
   
 <body>
     
-    <form class="form-register" action="../basededatos/controller_login.php" method="POST"> <!-- "action": define la ubicación (URL) donde se envían los datos que el formulario ha recopilado cuando se validan. "method": define con qué método HTTP se envían los datos.-->
-        <input class="controls" type="text" name="nombre" placeholder="Nombre">
+    <form class="form-register" action="basededatos/controller_login.php" method="post"> <!-- "action": define la ubicación (URL) donde se envían los datos que el formulario ha recopilado cuando se validan. "method": define con qué método HTTP se envían los datos.-->
+        <input class="controls" type="text" name="nombre" placeholder="Nombreeeeeee">
         <input class="controls" type="number" name="numeroDoc" placeholder="Número de documento">
         <input class="controls" type="email" name="correo" placeholder="Correo">                
         <input class="controls" type="password" name="pas" placeholder="Contraseña">
