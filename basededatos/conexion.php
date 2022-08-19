79df2c130d04b2c804fcad76268fc5e8b93a838d
Camilo Orduz <acorduz9@misena.edu.co>
2022-08-18 20:20:30 -0500

    conexion.php

--- basededatos/conexion.php
+++ basededatos/conexion.php
@@ -9,7 +9,7 @@ class DB{
         $username = 'u85yrqverx1h8saf';
         $password = 'M30qXRJgkOtmmkmOjwvS';
         
-        $conexion = new PDO('mysql:host=' + $localhost+'; dbname='+$database, $username, $password);
+        $conexion = new PDO('mysql:host=bkc3739td52nqn13e3iu-mysql.services.clever-cloud.com;dbname=bkc3739td52nqn13e3iu', $username, $password);
         return $conexion; 
     }
 }
