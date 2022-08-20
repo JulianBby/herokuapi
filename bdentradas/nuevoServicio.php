d6f05e149008635f8d7bb71ca33900227f6dcca6
Camilo-Orduz <acorduz9@misena.edu.co>
2022-08-20 11:33:00 -0500

    formulario

--- bdentradas/nuevoServicio.php
+++ bdentradas/nuevoServicio.php
@@ -21,22 +21,22 @@
                 <br>
                 <form action="nuevoServicio2.php" method="POST" style="border-collapse: separate; border-spacing: 10px 5px;">
                     <label for="">Nombre del Cliente: </label><br>
-                    <input type="text" name="nombreCliente" id="nombreCliente"><br><br><br>
+                    <input type="text" name="nombreCliente" id="nombreCliente"><br><br>
 
                     <label for="">Documento del Cliente: </label><br>
-                    <input type="number" id="documentoCliente" name="documentoCliente"><br><br><br>
+                    <input type="number" id="documentoCliente" name="documentoCliente"><br><br>
 
                     <label for="">Tipo de Prenda: </label><br>
-                    <input type="text" id="tipoPrenda" name="tipoPrenda"><br><br><br>
+                    <input type="text" id="tipoPrenda" name="tipoPrenda"><br><br>
 
                     <label for="">Servicio: </label><br>
-                    <input type="text" id="servicio" name="servicio"><br><br><br>
+                    <input type="text" id="servicio" name="servicio"><br><br>
 
                     <label for="">Precio: </label><br>
-                    <input type="number" name="precio" id="precio"><br><br><br>
+                    <input type="number" name="precio" id="precio"><br><br>
 
                     <label for="">Detalles: </label><br>
-                    <textarea name="detalles" id="detalles" cols="30" rows="10"></textarea><br><br><br>
+                    <textarea name="detalles" id="detalles" cols="30" rows="10"></textarea><br><br>
 
                     <label for="">Fecha de Entrada</label>  
                     <input type="date" name="fechaEntrada" id="fechaEntrada">
