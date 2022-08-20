e7b3b1aa0dafaa6c0496861f6feb8cb263604a2a
Camilo-Orduz <acorduz9@misena.edu.co>
2022-08-20 09:27:42 -0500

    registro.php

--- registro.php
+++ registro.php
@@ -11,11 +11,11 @@
 <body>
     
     <form class="form-register" action="basededatos/controller_login.php" method="post"> <!-- "action": define la ubicación (URL) donde se envían los datos que el formulario ha recopilado cuando se validan. "method": define con qué método HTTP se envían los datos.-->
-        <input class="controls" type="text" name="nombre" placeholder="Nombreeeeeee">
+        <input class="controls" type="text" name="nombre" placeholder="Nombre">
         <input class="controls" type="number" name="numeroDoc" placeholder="Número de documento">
         <input class="controls" type="email" name="correo" placeholder="Correo">                
         <input class="controls" type="password" name="pas" placeholder="Contraseña">
-        <center><input class="fcc-btn" type="submit" value="registrar" name="registrar"></center>
+        <center><input class="fcc-btn" type="submit" value="Registrar" name="registrar"></center>
     </form>
 
 </body>
