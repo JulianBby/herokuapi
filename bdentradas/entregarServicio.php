fecf2d44f0a07b952227ed60dc9b4ca69f6ca3e7
Camilo-Orduz <acorduz9@misena.edu.co>
2022-08-20 12:45:51 -0500

    lksajd

--- bdentradas/entregarServicio.php
+++ bdentradas/entregarServicio.php
@@ -33,14 +33,14 @@
                     <form action="entregarServicio2.php" method="POST" style="border-collapse: separate; border-spacing: 1px 5px;">
                         <input type="hidden" name="idEntrada" value="<?php echo $_GET['idEntrada']?>">
 
-                        <label for="">Precio: </label>
+                        <label for="">Precio: </label><br>
                         <input type="number" readonly="readonly" name="precio" id="precio" value="<?php echo $consulta[0] ?>"><br>
 
-                        <label for="">Fecha de Entrada</label>
+                        <label for="">Fecha de Entrada</label><br>
                         <input type="date" readonly="readonly" name="fechaEntrada" id="fechaEntrada" value="<?php echo $consulta[1] ?>">
                         
-                        <label for="">Fecha de Entrega</label>
-                        <input type="date" name="fechaEntrega" id="fechaEntrega"><br>
+                        <label for="">Fecha de Entrega</label><br>
+                        <input type="date" name="fechaEntrega" id="fechaEntrega"><br><br>
                         
                         <label for="">Observaciones</label><br>
                         <textarea name="observaciones" id="observaciones" cols="30" rows="10"></textarea>
